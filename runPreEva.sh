2f82ffc8766de12b12e932965458c290c224c25a
Peiyuan Zhai <peiyuanzhai@icdcs5.ewi.tudelft.nl>
2022-06-09 20:12:32 +0200

    add a sh file

---
+++ runPreEva.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# start background matlab run
+nohup matlab -nodisplay -nosplash -nodesktop -r "try;predictionEvaluation;sendMailExit;catch e;fprintf(1,'The identifier was:\n%s',e.identifier);fprintf(1,'There was an error! The message was:\n%s',e.message);sendErrMailExit;end" </dev/null >serverMatlabRunLog.txt 2>serverMatlabRunLog.err &
