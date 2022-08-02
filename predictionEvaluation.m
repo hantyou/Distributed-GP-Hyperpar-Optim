b99dee7b7a96b7a8ac59a5bd5eec320ae6189c09
hantyou <p.zhai-1@student.tudelft.nl>
2022-08-02 23:31:11 +0200

    update

--- predictionEvaluation.m
+++ predictionEvaluation.m
@@ -27,7 +27,7 @@ reso=[reso_m,reso_n];
 repeatNum=3;    
 %% Evaluation setup
     Ms=[2,4,8,12,16]; % different number of agents for different exp groups
-    tempFlag=[1,1,1,1,1];
+    tempFlag=[0,0,1,1,0];
     
     maxRange=max(range(:))-min(range(:));
     commuRange=[maxRange,maxRange/2,maxRange/3,maxRange/3,maxRange/7];
