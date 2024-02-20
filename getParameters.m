9a7bd94c3dd4626d38d57c5068fd2cfea7a3d919
pzhai1 <p.zhai@tudelft.nl>
2024-02-20 14:20:05 +0100

    update

---
+++ getParameters.m
@@ -0,0 +1,8 @@
+
+realDataSet=0;
+% 0: use 2D-GP artificial dataset;
+% 1: Use real dataset (Which is loaded by loadRealDataset)
+
+samplingMethod=1;
+% 1. uniformly distirbuted accross region;
+% 2. near agents position, could lose some points if out of range
